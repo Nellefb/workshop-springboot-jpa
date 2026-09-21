8941fce31adf6eeb84bacf99e79aeaf38ea1c212
Nellefb <ellenfebar@gmail.com>
2026-09-21 15:36:11 -0300

    Project created

---
+++ src/main/java/com/ellen/store_web_services/StoreWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.ellen.store_web_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ellen/store_web_services/StoreWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ellen.store_web_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
