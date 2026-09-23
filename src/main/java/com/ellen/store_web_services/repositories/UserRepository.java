11129ede647f9a5b32c19469ed7ec19037a8bf0c
Nellefb <ellenfebar@gmail.com>
2026-09-23 18:01:29 -0300

    Service layer and Component Registration

--- src/main/java/com/ellen/store_web_services/repositories/UserRepository.java
+++ src/main/java/com/ellen/store_web_services/repositories/UserRepository.java
@@ -3,5 +3,6 @@ package com.ellen.store_web_services.repositories;
 import com.ellen.store_web_services.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 }
